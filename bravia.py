0cbdeb22ec52cc09129918bd015dd227f3a40d24
Himanshu Attri <attri.him@gmail.com>
2016-09-24 01:01:22 +0530

    Control Menu Added

--- bravia.py
+++ bravia.py
@@ -3,9 +3,33 @@ from subprocess import call
 
 
 
-
+call(["clear"])
 while 1:
+	
 	print("Bravia :")
+	a='''               Bravia KDL43W950C Remote Control       EXIT ;
+
+
+Numbers= 0-9     Back Esc     
+Home h           ActionMenu O
+Select Enter     ChannelUp *
+Tv z             ChannelDown /	
+Power p          Input I
+VolumeUp +	 TvInput n
+VolumeDown -	 TvAntennaCable b
+Mute mCursorUp w WakeUp W
+CursorDown s	 Sleep S
+CursorRight d	 Right R
+CursorLeft a	 Left L
+Help i		 SleepTimer U
+HDMI1 Z 	 TvAnalog !
+HDMI2 X 	 Analog2 @	
+HDMI3 C 
+HDMI4 V
+
+
+'''
+	print a
 	
 	a=repr(readchar.readchar())
 	print a
@@ -29,4 +53,3 @@ while 1:
 	call(["clear"])
 
 	
-
