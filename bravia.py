469f51bc1260daae3e1d1ca0942998f7a0ae175c
Himanshu Attri <attri.him@gmail.com>
2016-09-24 02:22:19 +0530

    Update bravia.py

--- bravia.py
+++ bravia.py
@@ -1,12 +1,31 @@
 import readchar
 from subprocess import call
 
+call(["clear"])
+print '''
+
+		Linux-Bravia_Remote By: Himanshu Attri
+		  (https://github.com/HimanshuAttri}
+
+
+Instructions
+1. Navigate to: Settingsn Network  Home Network Setup IP Control
+2. Set Authentication to Normal and Pre Shared Key
+3. There should be a new menu entry Pre-Shared Key. Set it to **0000**. 
+   If you choose anything else, you need to change the PSK in the `send_command.   sh` script.
+
+
+
+
+Bravia Tv Local ip: '''
+ip=raw_input()
+
 
 
 call(["clear"])
 while 1:
 	
-	print("Bravia :")
+	print("Bravia : "+ip)
 	a='''               Bravia KDL43W950C Remote Control       EXIT ;
 
 
@@ -17,7 +36,8 @@ Tv z             ChannelDown /
 Power p          Input I
 VolumeUp +	 TvInput n
 VolumeDown -	 TvAntennaCable b
-Mute mCursorUp w WakeUp W
+Mute m           WakeUp W
+CursorUp w          
 CursorDown s	 Sleep S
 CursorRight d	 Right R
 CursorLeft a	 Left L
@@ -26,8 +46,7 @@ HDMI1 Z 	 TvAnalog !
 HDMI2 X 	 Analog2 @	
 HDMI3 C 
 HDMI4 V
-
-
+							By: Himanshu Attri 
 '''
 	print a
 	
@@ -38,7 +57,7 @@ HDMI4 V
 		exit()
 	if( a=="'\'"):
 	 	b=raw_input()
-		call(["./send_command.sh", b])
+		call(["./send_command.sh", b ,ip])
 		call(["clear"])
 
 	if( a=="']'"):
@@ -47,7 +66,7 @@ HDMI4 V
 		call(["clear"])
 
 	else:
-		call(["./send_command.sh", a])
+		call(["./send_command.sh", a ,ip])
 		call(["clear"])
 #	call(["./ad.sh"])
 	call(["clear"])
