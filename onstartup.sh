ab0903054bf9066689a0e616e051b6442d607a1d
HimanshuAttri <attri.him@gmail.com>
2017-10-10 06:02:24 +0530

    edited

---
+++ onstartup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#To start run remote on startup
+
+python pathto-BraviaGUI.py &
+#replace actual pathto with actual path of BraviaGUI.py
+# enable run.sh with chmod 777
+#add run.sh to startup Applications <option in settings>
+
